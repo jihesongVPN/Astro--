49333df3c2bec8c78f603cea16b7f3d2dfefcde6
jihesongVPN <138542113+jihesongVPN@users.noreply.github.com>
2025-08-23 08:22:29 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "化风行万里",
+	description: "Nothing",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "zh-CN",
 	// Meta property, found in src/components/BaseHead.astro L:42
